3cea39299d34fdbed17d1f3e88084473f234d5f2
shturmovick <klaoner@gmail.com>
2025-06-02 22:19:25 +0300

    Добавил работу с переменными

---
+++ variables.py
@@ -0,0 +1,13 @@
+
+my_name = "Merk"
+my_age = 22
+height = 1.80
+is_student = False
+
+print("Меня зовут", my_name, "| Тип:", type(my_name))
+print("Мой возраст:", my_age, "| Тип:", type(my_age))
+print("Мой рост:", height, "| Тип:", type(height))
+print("Я студент?", is_student, "| Тип:", type(is_student))
+
+height_v5 = height - 0.80
+print("в 5 мой рост был", height_v5)
